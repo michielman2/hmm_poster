268019490a75b3fdceeaea60904a729a2710b51e
michielman2 <michiel.meeuwisse@gmail.com>
2025-01-27 13:45:02 +0100

    Delete hmm_shit directory

--- hmm_shit/hmm.py
+++
@@ -1,7 +0,0 @@
-def train(folder):
-    startlist = []
-    sequence = []
-    filename = 'data/cancer/AF008216.1.fasta'
-    with open(filename, "r") as file:
-        for line in file:
-            sequence += line
